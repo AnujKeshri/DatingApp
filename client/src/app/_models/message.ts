0e22b0a5cc9acdae88e8605f4da94d62cdf4d0a9
Anuj <me.anujy2j@gmail.com>
2022-07-09 23:16:25 +0800

    End of Section 15 - Adding the messaging feature

---
+++ client/src/app/_models/message.ts
@@ -0,0 +1,12 @@
+export interface IMessage {
+    id: number;
+    senderId: number;
+    senderUsername: string;
+    senderPhotUrl: string;
+    recipientId: number;
+    recipientUsername: string;
+    recipientPhotoUrl: string;
+    content: string;
+    dateRead?: Date;
+    messageSend: Date;
+}
---
+++ client/src/app/_resolvers/member-detailed.resolver.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+import { IMember } from '../_models/member';
+import { MembersService } from '../_services/members.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MemberDetailedResolver implements Resolve<IMember> {
+
+  constructor(private memberService: MembersService) { }
+
+  resolve(route: ActivatedRouteSnapshot): Observable<IMember> {
+    return this.memberService.getMember(route.paramMap.get('username') || '');
+  }
+}
